98e8beeee5f91ebec5784a0cc7f7af53eb32be72
jhareeshkumar <jhareesh30@gmail.com>
2025-02-26 17:08:56 +0530

    added logging and disabled swagger ui in prod and added log files to git ignore.

--- com.kodbook/src/main/java/com/kodbook/auth/v2/dto/ChangePasswordRequest.java
+++ com.kodbook/src/main/java/com/kodbook/auth/v2/dto/ChangePasswordRequest.java
@@ -10,14 +10,17 @@ import lombok.Setter;
 @Setter
 @Schema(description = "Request to change password.")
 public class ChangePasswordRequest {
+    @Schema(description = "Current password of the user", format = "password",example = "P@ssw0rd")
     @NotBlank(message = "Old password is required.")
     private String oldPassword;
-    
+
+    @Schema(description = "New password to be set", format = "password",example = "newP@ssw0rd")
     @NotBlank(message = "New password is required.")
-    @Size (min = 6, message = "New password must be at least 6 characters long.")
+    @Size (min = 6, max = 20 ,message = "New password must be at least 6 characters long.")
     private String newPassword;
-    
+
+    @Schema(description = "OTP for password change", format = "OTP",example = "123456")
     @NotBlank(message = "OTP is required")
-    @Size(min = 6, max = 6, message = "OTP must be 6 characters long.")
+    @Size(min = 6, max = 6, message = "OTP must be 6 digits")
     private String otp;
 }
