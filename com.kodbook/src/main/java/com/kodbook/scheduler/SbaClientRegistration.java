1e2e25be0a3596f0c6f591123b44c98d37681f48
jhareeshkumar <jhareesh30@gmail.com>
2025-04-13 00:38:26 +0530

    bugFix: Client registration to admin fixed with delay #49

---
+++ com.kodbook/src/main/java/com/kodbook/scheduler/SbaClientRegistration.java
@@ -0,0 +1,29 @@
+package com.kodbook.scheduler;
+
+import de.codecentric.boot.admin.client.registration.ApplicationRegistrator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+@EnableScheduling
+@RequiredArgsConstructor
+public class SbaClientRegistration {
+
+    private final ApplicationRegistrator applicationRegistrator;
+
+    @Scheduled(initialDelay = 2, fixedDelay = 2, timeUnit = TimeUnit.MINUTES)
+    public void scheduleClientRegistration() {
+        boolean registerStatus = applicationRegistrator.register();
+        if (registerStatus) {
+            log.info("SBA client registered successfully");
+        } else {
+            log.error("SBA client registration failed");
+        }
+    }
+}
