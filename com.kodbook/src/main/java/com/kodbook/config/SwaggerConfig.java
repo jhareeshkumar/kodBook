070d74dbb296769d0cd7e1289b5cee82bb801163
jhareeshkumar <jhareesh30@gmail.com>
2025-05-19 00:03:29 +0530

    Removed the servers from SwaggerConfig.java to generate it automatically.

--- com.kodbook/src/main/java/com/kodbook/config/SwaggerConfig.java
+++ com.kodbook/src/main/java/com/kodbook/config/SwaggerConfig.java
@@ -6,11 +6,8 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -27,10 +24,6 @@ public class SwaggerConfig {
                                 .url("https://kodbook.com")
                         )
                 )
-                .servers(List.of(
-                        new Server().url("http://localhost:8081").description("Dev Server"),
-                        new Server().url("https://kodbook.com").description("Prod Server")
-                ))
                 .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
                 .components(new Components()
                         .addSecuritySchemes("basicAuth", new SecurityScheme()
