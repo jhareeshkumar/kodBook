18f22e1d5f57942bef32479d5a5c339bae3f9a64
jhareeshkumar <jhareesh30@gmail.com>
2025-05-20 00:25:03 +0530

    Restructured project layout to standard Spring Boot format #69

--- com.kodbook/src/main/java/com/kodbook/auth/exception/IncorrectPasswordException.java
+++
@@ -1,8 +0,0 @@
-package com.kodbook.auth.exception;
-
-public class IncorrectPasswordException extends RuntimeException {
-
-    public IncorrectPasswordException(String message) {
-        super(message);
-    }
-}
